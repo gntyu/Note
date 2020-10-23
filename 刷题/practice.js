3c8646de11e7872122396430aeb5e78cef98e392
鲁燕 <luyan1@staff.weibo.com>
2020-10-23 17:20:24 +0800

    .

--- 刷题/practice.js
+++ 刷题/practice.js
@@ -1,27 +1,19 @@
-// ~function () {
-//   String.prototype.myReplace = function (T,callback) {
-//     T = new RegExp(T,'g')
-//     let res = '', i = 0
-//     for (let result = T.exec(this); result !== null; result = T.exec(this)){
-//       console.log(result)
-//       res += this.substring(i,result.index) + callback(...result)
-//       i = result.index+result[0].length
-//     }
-//     // while ((result = T.exec(this)) !== null) {
-//     //   console.log(result)
-//     //   res += this.substring(i,result.index) + callback(...result)
-//     //   i = result.index+result[0].length
-//     // }
-//     res += this.substring(i)
-//     return res
-//   }
-// }()
-// let S = "qinzhenWEN是个大傻子！。~kdkWEN...WENksjdfch两居室的"
-// let T = 'WEN'
-// console.log(S.myReplace(T, (...item) => {
-//   return "@"
-// }))
-let obj = JSON.parse("{ \"a\": 1 }")
-console.log(obj)
+function Foo(){
+  Foo.a = function () {
+    console.log(1)
+  }
+  this.a = function () {
+    console.log(2)
+  }
+}
+Foo.prototype.a = function () {
+  console.log(3)
+}
+Foo.a = function () {
+  console.log(4)
+}
 
-
+Foo.a()//4
+let obj = new Foo()//new 的时候 => Foo.a:f=>1 a:f=>2
+obj.a()//2   
+Foo.a()//4错了，应该是1
