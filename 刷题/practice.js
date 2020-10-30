ea2b270fa25e9172193424ff905bd8fd9dc1352e
鲁燕 <luyan1@staff.weibo.com>
2020-10-30 10:01:11 +0800

    .

--- 刷题/practice.js
+++ 刷题/practice.js
@@ -1,19 +1,4 @@
-function Foo(){
-  Foo.a = function () {
-    console.log(1)
-  }
-  this.a = function () {
-    console.log(2)
-  }
+var a = ?;//当a的值是什么时，下面条件成立输出1
+if (a == 1 && a == 2 && a == 3) {
+  console.log(1)
 }
-Foo.prototype.a = function () {
-  console.log(3)
-}
-Foo.a = function () {
-  console.log(4)
-}
-
-Foo.a()//4
-let obj = new Foo()//new 的时候 => Foo.a:f=>1 a:f=>2
-obj.a()//2   
-Foo.a()//4错了，应该是1
